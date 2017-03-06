dc7b3956731773f8f91b34726792c21d4af2f251
LinguisticsStudent <student.linguistics@gmail.com>
2017-03-06 11:53:46 -0500

    Update twitterbot_textfile.py

--- twitterbot_textfile.py
+++ twitterbot_textfile.py
@@ -21,19 +21,23 @@ file_lines = my_file.readlines()
 # Close file
 my_file.close()
 
+# Tweet a line every 5 seconds
+def tweet():
 # Create a for loop to iterate over file_lines
-for line in file_lines:
+    for line in file_lines:
 # Add try ... except block to catch and output errors
-    try:
-        print(line)
-	# Add if statement to ensure that blank lines are skipped
-        if line != '\n':
-            api.update_status(line)
-
-	# Add an else statement with pass to conclude the conditional statement
-        else:
-            pass
-    except tweepy.TweepError as e:
-        print(e.reason)
-# Add sleep method to space tweets by 5 seconds each
-    sleep(5)
+        try:
+            print(line)
+# Add if statement to ensure that blank lines are skipped
+            if line != '\n':
+                api.update_status(line)
+				sleep(5)
+# Add an else statement with pass to conclude the conditional statement
+            else:
+                pass
+        except tweepy.TweepError as e:
+            print(e.reason)
+# Add sleep method to space tweets by 2 seconds each
+            sleep(2)
+			
+tweet()
