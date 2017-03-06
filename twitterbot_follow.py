df00b887adaadbf3fc0daa4f33178544d73ea001
LinguisticsStudent <student.linguistics@gmail.com>
2017-03-06 14:30:49 -0500

    Create twitterbot_follow.py

---
+++ twitterbot_follow.py
@@ -0,0 +1,27 @@
+# Reference: https://dototot.com/write-twitter-bot-python-tweepy-unfollow-non-followers-command-line/
+
+# Import Tweepy, sleep, credentials.py
+import tweepy
+from time import sleep
+from credentials import *
+
+# Access and authorize our Twitter credentials from credentials.py
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+SCREEN_NAME = "Counter10000"
+
+# Unfollow non-followers
+followers = api.followers_ids(SCREEN_NAME)
+friends = api.friends_ids(SCREEN_NAME)
+ 
+for f in friends:
+    if f not in followers:
+        print "Unfollow {0}?".format(api.get_user(f).screen_name)
+        if raw_input("Y/N?") == 'y' or 'Y':
+            api.destroy_friendship(f)
+			
+# Follow new followers
+for follower in tweepy.Cursor(api.followers).items():
+    follower.follow()
+	print follower.screen_name
