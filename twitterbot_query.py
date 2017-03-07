7c07da3b0662420dba70308ef53d4acf560c1080
LinguisticsStudent <student.linguistics@gmail.com>
2017-03-06 22:52:29 -0500

    Update twitterbot_query.py

--- twitterbot_query.py
+++ twitterbot_query.py
@@ -8,5 +8,7 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-for tweet in tweepy.Cursor(api.search, screen_name=USER, q="KEYWORD", since="2017-03-04", until="2017-03-05", lang="en").items(20):
-    print('Tweet by: @' + tweet.user.screen_name + ' about KEYWORD: ' + tweet.text)
+SCREEN_NAME = "WallStJesus"
+KEYWORD = "$BAC"
+for tweet in tweepy.Cursor(api.search, screen_name=SCREEN_NAME, q=KEYWORD, since="2017-02-07", until="2017-03-05", lang="en").items(20):
+    print('Tweet by: @' + tweet.user.screen_name + ' about KEYWORD: ' + KEYWORD + tweet.text)
