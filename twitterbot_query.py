086eb6f4b23f997caf49d6f851188cea5d67c344
Sophie's  GitHub <student.linguistics@gmail.com>
2017-03-07 15:01:58 -0500

    Update twitterbot_query.py

--- twitterbot_query.py
+++ twitterbot_query.py
@@ -10,5 +10,7 @@ api = tweepy.API(auth)
 
 SCREEN_NAME = "WallStJesus"
 KEYWORD = "$BAC"
-for tweet in tweepy.Cursor(api.search, screen_name=SCREEN_NAME, q=KEYWORD, since="2017-02-07", until="2017-03-05", lang="en").items(20):
-    print('Tweet by: @' + tweet.user.screen_name + ' about KEYWORD: ' + KEYWORD + tweet.text)
+# for tweet in tweepy.Cursor(api.search, screen_name=SCREEN_NAME, q=KEYWORD, since="2017-02-07", until="2017-03-05", lang="en").items(20):
+    # print('Tweet by: @' + tweet.user.screen_name + ' about KEYWORD: ' + KEYWORD + tweet.text)
+for tweet in tweepy.Cursor(api.search, q=KEYWORD, lang="en").items(20):
+    print 'tweet:' + tweet + '/n tweet.text:' + tweet.text + '/n tweet.from_user:' + tweet.from_user + '/n tweet.screen_name:' + tweet.screen_name + '/n tweet.time:' + tweet.time
