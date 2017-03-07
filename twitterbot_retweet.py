4ebc02fffd957860cb6296edd153a97879ea0ca6
LinguisticsStudent <student.linguistics@gmail.com>
2017-03-06 19:48:32 -0500

    Update twitterbot_retweet.py

--- twitterbot_retweet.py
+++ twitterbot_retweet.py
@@ -10,30 +10,26 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# For loop to iterate over tweets with #ocean, limit to 10
-for tweet in tweepy.Cursor(api.search, q='#ocean').items(10):
+# For loop to iterate over tweets with #churchsign, limit to 10
+for tweet in tweepy.Cursor(api.search, q='#churchsign').items(10):
     try:
-# Print out usernames of the last 10 people to use #ocean
+# Print out usernames of the last 10 people to use #churchsign
 # Add \n escape character to print() to organize tweets
-	    print('\nTweet by: @' + tweet.user.screen_name)
-    
+        print('\n Tweet by: @' + tweet.user.screen_name)
+        sleep(5)
 # Retweet tweets as they are found
         tweet.retweet()
-		print('Retweeted the tweet')
+        print('Retweeted the tweet')
 
 # Favorite the tweet
         tweet.favorite()
-		print('Favorited the tweet')
+        print('Favorited the tweet')
 
 # Follow the user who tweeted
         if not tweet.user.following:
-		    tweet.user.follow()
-		    print('Followed the user')
-
-        sleep(5)
-		
-	except tweepy.TweepError as e:
-	    print(e.reason)
-		
+            tweet.user.follow()
+            print('Followed the user')
+    except tweepy.TweepError as e:
+        print(e.reason)
     except StopIteration:
-	    break
+        break
